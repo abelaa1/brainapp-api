8b5b52b943d5631d6c322805f66ed1d7e95d8a29
Abel <aguilarabel165@gmail.com>
2020-12-27 18:20:57 -0800

    api

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors')
+const knex = require('knex');
+const { response } = require('express');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client:'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'postgres',
+        database: 'smart-brain'
+    }
+});
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req,res)=>{ res.send(database.users)})
+app.post('/signin',(req, res) => {signin.handleSignin(req, res, db, bcrypt) })
+app.post('/register',(req, res) => {register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db) })
+app.put('/image', (req, res) => {image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res) })
+
+app.listen(3003, ()=>{
+    console.log('app is running');
+})
+
+
+// bcrypt.hash("bacon", null, null, function(err, hash) {
+//     // Store hash in your password DB.
+// });
+
+// // Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//     // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//     // res = false
+// });
+
+/*
+/--> res = this working
+/signin --> POST = succes/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+*/
